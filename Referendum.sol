653ddc372c86e6be4163d02f9d21b2d1ad3c97fa
felipem4rtins <36815372+felipem4rtins@users.noreply.github.com>
2020-12-16 14:33:19 -0300

    Update Referendum.sol

--- Referendum.sol
+++ Referendum.sol
@@ -8,47 +8,88 @@ contract Referendum {
         uint totalVotes;
     }
     
+    enum Stages {
+        Voting,
+        Revealing,
+        Finished
+    }
+     
+     
+    
      // Store accounts that have voted
      mapping(address => bool) public voters;
      Result result;
-     string public subject;
+     string subject;
      address owner;
      uint referendumEnd;
-     uint minVotos;
+     uint maxVotes;
+     uint duration;
+     Stages private stage = Stages.Voting;
+     uint private creationTime = now;
     
      constructor(string _subject,
-        uint _referedumDurationInDays, uint _minVotos) public {
+        uint _referedumDurationInDays, uint _maxVotes) public {
         subject = _subject;
-        minVotos = _minVotos;
+        maxVotes = _maxVotes;
         owner = msg.sender;
         result = Result(0, 0, 0);
-        referendumEnd = now + _referedumDurationInDays * 5 minutes;
+        duration =  _referedumDurationInDays * 1 minutes;
     }
     
-    modifier onlyAfter(uint _time) {
-        require(now >= _time);
-        _;
-    }
     
-    modifier onlyWith(uint _votes) {
-        require(_votes >= minVotos);
+    modifier atStage(Stages _stage) {
+        if(stage == Stages(0))
+        {
+            require(stage == _stage,"Currently voting");
+        }
+        if(stage == Stages(1))
+        {
+            require(stage == _stage,"Awaiting reveal");
+        }
+        if(stage == Stages(2))
+        {
+            require(stage == _stage,"Finished");
+        }
         _;
     }
 
-    function Reveal()
-        public
-        onlyAfter(referendumEnd)
-        onlyWith(result.totalVotes)
-        view returns(string)
-    {
-            if(result.yes>result.no) return "yes";
-            if(result.no>result.yes) return "no";
-            else return "draw";
+    function nextStage() internal {
+        stage = Stages(uint(stage) + 1);
     }
     
-     function vote (uint _vote) private {
+    modifier timedTransitions() {
+        if (stage == Stages.Voting &&
+                    now >= creationTime + duration)
+            nextStage();
+        // The other stages transition by transaction
+        _;
+    }
+    
+    function StagePrint() public view returns(Referendum.Stages)
+    {
+        return Stages(uint(stage));
+    }
+
+    function Reveal() timedTransitions atStage(Stages.Revealing) public returns(string)
+        {
+            nextStage();
+            if(result.totalVotes < (maxVotes*2)/3)
+            {
+                return "Not enough votes";
+            }
+            else
+            {
+                if(result.yes>result.no) return "yes";
+                if(result.no>result.yes) return "no";
+                else return "draw";
+            }
+        }
+        
+        
+    
+     function vote (uint _vote) timedTransitions atStage(Stages.Voting) private {
      // require that they haven't voted before
-     require(!voters[msg.sender]);
+     require(!voters[msg.sender],"Already voted");
     
      // require a valid candidate
      require(_vote == 0 || _vote == 1);
@@ -60,6 +101,10 @@ contract Referendum {
      if (_vote == 1) result.yes++;
      else result.no++;
      result.totalVotes++;
+     if(result.totalVotes==maxVotes)
+     {
+         nextStage();
+     }
      }
      
      function voteYes() public{
@@ -71,10 +116,13 @@ contract Referendum {
      function GetSubject() public view returns (string){
          return subject;
      }
-     function GetReferendumEnd() public view returns (uint){
-         return referendumEnd;
+     function GetYes() public view returns (uint){
+         return result.yes;
      }
-     function GetMinVotos() public view returns (uint){
-         return minVotos;
+     function GetNo() public view returns (uint){
+         return result.no;
+     }
+     function GetTotal() public view returns (uint){
+         return result.totalVotes;
      }
 }
